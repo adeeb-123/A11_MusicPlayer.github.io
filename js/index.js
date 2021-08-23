16d8e835de408314e2cdbc2a0efe1e4e097fd68c
Adeeb Ahmad <73013690+adeeb-123@users.noreply.github.com>
2021-08-23 17:20:14 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -102,5 +102,66 @@ function prevsong() {
     playsong()
 };
 
+
+// progress bar logic
+
+let progress = document.getElementById('progress')
+let total_duration = document.getElementById('duration')
+let current_time = document.getElementById('current_time')
+
+
+song.addEventListener('timeupdate',(event)=>{
+    // console.log(event)
+    const {currentTime , duration} = event.srcElement;
+    // console.log(`current time is ${currentTime}`)
+    // console.log(`Duration time is ${duration}`)
+    
+    let progress_time = (currentTime / duration) * 100;
+    progress.style.width = `${progress_time}%`
+
+    // ------------------> Duration Time update <-----------------
+
+    let minute_duration = Math.floor(duration / 60);  // converted into minute
+    let second_duration = Math.floor(duration % 60); // converted into second
+
+    // console.log(minute_duration)
+    // console.log(second_duration)
+
+    let tot_duration = `${minute_duration}:${second_duration}`;
+    if(duration){
+
+        total_duration.textContent = `${tot_duration}`
+    }
+
+    // ------------------> Current Time update <-----------------
+    let minute_currentTime = Math.floor(currentTime / 60);  // converted into minute
+    let second_currentTime = Math.floor(currentTime % 60); // converted into second
+
+    // console.log(minute_duration)
+    // console.log(second_duration)
+
+    if(second_currentTime < 10){
+        second_currentTime = `0${second_currentTime}`
+    }
+    let tot_currentTime = `${minute_currentTime}:${second_currentTime}`;
+    current_time.textContent = `${tot_currentTime}`
+});
+
+// whenever someone clicks on the progress bar song shoul starts from there
+const progress_div = document.getElementById('progress_div')
+
+progress_div.addEventListener('click' ,(event)=>{
+    // console.log(event)
+    const {duration} = song;
+
+    let move_progress = (event.offsetX / event.srcElement.clientWidth) * duration;
+    console.log(move_progress)
+
+    song.currentTime = move_progress  // currentTime is a property (google it for details)
+})
+
+// ----------------------------> if music ends we want to play the next song ----------------------<
+song.addEventListener('ended',  nextsong); // song end apply the nextsong function
+
 next_btn.addEventListener('click', nextsong);
 prev_btn.addEventListener('click', prevsong);
